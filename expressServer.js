96a5b348255cb5d9198282fde7e022853e291093
Brandon Martin <brandonmartin@Brandons-MacBook-Pro.local>
2022-08-19 15:15:46 -0600

    added small changes

--- expressServer.js
+++ expressServer.js
@@ -34,18 +34,56 @@ app.get('/pets/:id', function(req, res) {
         
         res.send(sendData)
     })
-    if(indexValue > lastIndexValue.length - 2) {
+    if(indexValue >= lastIndexValue.length - 1) {
         res.status(404);
         res.setHeader('Content-Type', 'text/plain')
-        res.end('404 Not Found')
+        res.send('404 Not Found')
     } else if(indexValue < 0){
         res.status(404);
         res.setHeader('Content-Type', 'text/plain')
-        res.end('404 Not Found')
+        res.send('404 Not Found')
 
     }
 
 })
+
+// app.post('/pets', function(req, res) {
+
+//     fs.readFile('pets.json', 'utf8', function(req, res) {
+//         if(err) {
+//             console.log('error')
+//         }
+//         const parseData = JSON.parse(data)
+//         console.log(parseData)
+//         parseData.push(createCommand)
+
+//         const JSONcreate = JSON.stringify(parseData)
+//         let age = parseInt(process.argv[3])
+//         let kind = process.argv[4]
+//         let name = process.argv[5]
+//         res.send(age, kind, name)
+//         if(!age || !kind || !name){
+//             console.log("need more info")
+//         } else {
+//             fs.writeFile('pets.json', JSONcreate, function(){
+//                 console.log('error')
+//             })
+//         }
+//     })
+
+
+//     // let age = parseInt(process.argv[3])
+//     // let kind = process.argv[4]
+//     // let name = process.argv[5]
+//     // res.send(age, kind, name)
+//     // if(!age || !kind || !name){
+//     //     console.log("need more info")
+//     // } else {
+//     //     fs.writeFile('pets.json', JSONcreate, function(){
+//     //         console.log('error')
+//     //     })
+//     // }
+// })
 
 
 app.listen(PORT, function() {
